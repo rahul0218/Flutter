752ba060b2a2b0332d2387b89f6ac71660c0ea69
Rahul kumar <kumar.rahul0218@gmail.com>
2020-09-26 15:06:17 +0530

    Flutter WordPairApp full

---
+++ wordpair_generator/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //final wordPair = WordPair.random();
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]), home: RandomWords()
+        /* Scaffold(
+            appBar:
+                AppBar(centerTitle: true, title: Text('WordPair Generator')),
+            body: Center(
+              child: Text(wordPair.asPascalCase)
+            )
+            ) */
+
+        );
+  }
+}
+
