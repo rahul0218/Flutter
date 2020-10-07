a35ef685d1fdda52dc66e091736e9383c9659066
Rahul kumar <kumar.rahul0218@gmail.com>
2020-10-07 12:40:10 +0530

    Widget Manage its Own Satate

---
+++ Layout/WidgetManageOwnState/android/app/src/main/kotlin/com/example/WidgetManageOwnState/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.WidgetManageOwnState
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Layout/WidgetManageOwnState/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class TapboxA extends StatefulWidget {
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+          style: TextStyle(fontSize: 32.0, color: Colors.white)
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(appBar: AppBar(title: Text('Example of Widget Manages it\'s own State')),body: Center(child: TapboxA(),),),
+    );
+  }
+}
