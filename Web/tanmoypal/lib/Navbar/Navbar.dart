f49c894ffc370bfd3def7a30f3f3025fd9980b08
Rahul Kumar <37036894+rahul0218@users.noreply.github.com>
2020-12-28 20:47:21 +0530

    logo modification

--- Web/tanmoypal/lib/Navbar/Navbar.dart
+++ Web/tanmoypal/lib/Navbar/Navbar.dart
@@ -1,43 +1,65 @@
 import 'package:flutter/material.dart';
-class  Navbar extends StatelessWidget {
+
+class Navbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-      builder:(context,constraints){
-        if(constraints.maxWidth>1200){
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 1200) {
           return DesktopNavbar();
-        }else if(constraints.maxWidth>800 && constraints.maxWidth<1200){
+        } else if (constraints.maxWidth > 800 && constraints.maxWidth < 1200) {
           return DesktopNavbar();
-        }else{
+        } else {
           return MobileNavbar();
         }
       },
     );
   }
 }
+
 class DesktopNavbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.symmetric(vertical: 20, horizontal:60),
-    child: Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Image.asset("assets/images/logo.png",),
-        
-        
-         Row(
-           children: [
-              Text("Resume", style: TextStyle(color:Colors.black),),
-              SizedBox(width:30,),
-              Text("Work", style: TextStyle(color:Colors.black),),
-              
-              
-         ],)
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 60),
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Image.asset(
+              "assets/images/logo@2x.png",
+              height: 100,
+              width: 100,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Resume",
+                  style: TextStyle(color: Colors.black),
+                ),
+                SizedBox(
+                  width: 60,
+                ),
+                Text(
+                  "Work",
+                  style: TextStyle(color: Colors.black),
+                ),
+                SizedBox(
+                  width: 60,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
-    ),
     );
+  }
+}
+
+class DesktopbotNavbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
 
@@ -45,21 +67,30 @@ class MobileNavbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20, horizontal:60),
-      child: Column(children: [
-        Image.asset("assets/images/logo.png",),
-        
-        
-         Row(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-              Text("Resume", style: TextStyle(color:Colors.black),),
-              SizedBox(width:30,),
-              Text("Work", style: TextStyle(color:Colors.black),),
-              
-              
-         ],)
-      ],),
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 60),
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/logo.png",
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Resume",
+                style: TextStyle(color: Colors.black),
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              Text(
+                "Work",
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
